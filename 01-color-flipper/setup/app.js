8d53164fbeaa316e55e5d4ea0571d1907bfd8784
Harshit Tiwari <tiwariharshit909@gmail.com>
2022-12-12 17:36:48 +0530

    hex part of the project is also completed hence complete project is completed

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,15 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
-const btn = document.getElementById("btn");
+const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
-  // generate random number between 0 and the size of the array
-  const randomNumber = getRandomNumbers();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+btn.addEventListener("click", function(){
+    // generate random number between 0 and the size of the array
+    const randomNumber = getRandomNumbers();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 });
 
-function getRandomNumbers() {
-  return Math.floor(Math.random() * colors.length);
+function getRandomNumbers(){
+    return Math.floor(Math.random()*colors.length);
 }
